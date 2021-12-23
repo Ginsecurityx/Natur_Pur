f0f277f79e12755f2f62feee458dc9a8bf999982
Ginsecurity <syrelennart@gmail.com>
2021-12-24 00:37:12 +0100

    header, social-icons farben variablen, navi burger-animation

--- src/scripts/eventListeners.js
+++ src/scripts/eventListeners.js
@@ -3,11 +3,20 @@ window.onload = function() {
      //navigation
      const navButton = document.getElementById('nav-button');
      const nav = document.getElementById('navigation');
+     const bar1 = document.getElementById('burger-bar-1')
+     const bar2 = document.getElementById('burger-bar-2')
+     const bar3 = document.getElementById('burger-bar-3')
      
      navButton.onclick = function(){navToggle()};
-
+     
      function navToggle(){
+          //toggle nav
           nav.classList.toggle("open");
+
+          //hamburger animation
+          bar1.classList.toggle("active");
+          bar2.classList.toggle("active");
+          bar3.classList.toggle("active");
      };
 
 }
